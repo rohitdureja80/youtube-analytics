eda4f1f4b1d5458a61a873520f2452e36e5d8913
Rohit Dureja <rdureja@gmail.com>
2023-11-15 13:47:20 -0500

    adding insert date columns to df

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import Services.YouTubeApi as y
 import Database.Postgres as database
 from pandas import json_normalize
 import json
+import datetime
 
 def main():
     youtube = y.YouTubeApi()
     response = youtube.GetVideoCategories()
+    now = datetime.datetime.now()
     df = json_normalize(response["items"])
+    df.insert(0, "insertDate", now)
     db = database.Postgres()
     db.LoadDataFrame(data=df, tableName="VideoCategoriesRaw", boolReplace=True)
     for item in response["items"]:
@@ -15,8 +18,9 @@ def main():
         videos = youtube.GetMostPopularVideos(categoryId=categoryId, numResults=25)
         json_string = json.dumps(videos, indent=2) 
         #print(json_string)
-        df["videoCategoryId"] = categoryId
         df = json_normalize(videos["items"])
+        df.insert(0, 'videoCategoryId', categoryId)
+        df.insert(0, "insertDate", now)
         #print(df)
         db.LoadDataFrame(data=df, tableName="MostPopularVideosRaw", boolReplace=False)
 
